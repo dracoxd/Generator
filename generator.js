dcf1052f695aeaa18d73cf63a0d6c50354bdd2d5
ClusivePlayz <48451500+ClusivePlayz@users.noreply.github.com>
2019-03-15 03:16:04 +0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -535,6 +535,17 @@ function clusive(user, time) {
             message.channel.send(embed);
           skanherooo(message.author, 300);
             }
+          
+            if (message.content.startsWith ("+gen fortnite") && message.member.hasPermission ("VIEW_AUDIT_LOG")) {
+            if (message.channel.id !== '555903662916894749') return
+            if (message.author.bot || coolguy.has(message.author)) return
+            mention.send('**Your Fortnite Account Is: **' + a[Math.floor(Math.random() * 34)])
+            embed = new discord.RichEmbed ()
+            .setDescription ("I Have Successfully Sent You The Fortnite Account ! Please Check Your DMs:thumbup:")
+            .setColor ("#FF00FF")
+            message.channel.send(embed);
+          skanherooo(message.author, 300);
+            }
   
               if (message.content.startsWith ("+pgen fortnite") && message.member.hasPermission ("USE_EXTERNAL_EMOJIS")) {
                 if (message.channel.id !== '551484526262747144') return
@@ -547,6 +558,17 @@ function clusive(user, time) {
               cooldown(message.author, 180);
                 } 
 
+  if (message.content.startsWith ("+pgen fortnite") && message.member.hasPermission ("USE_EXTERNAL_EMOJIS")) {
+                if (message.channel.id !== '555903664209002496') return
+                if (message.author.bot || cooldowns.has(message.author)) return
+                mention.send('**Your Premuim Fortnite Account Is: **' + b[Math.floor(Math.random() * 40)])
+                embed = new discord.RichEmbed ()
+                    .setDescription ("I Have Successfully Sent You The Premuim Fortnite Account ! Please Check Your DMs:thumbup:")
+                    .setColor ("#FF00FF")
+                    message.channel.send(embed);
+              cooldown(message.author, 180);
+                } 
+  
             if (message.content.startsWith ("+gen spotify") && message.member.hasPermission ("VIEW_AUDIT_LOG")) {
             if (message.channel.id !== '551484524941410334') return
             if (message.author.bot || coolguys.has(message.author)) return
